e00645a4fcfaf2379099c909751f85ae1d8ce934
Bishram <m.bishram.yashir@gmail.com>
2025-12-25 08:31:27 +0800

    Add close button to alert component using alpinejs

--- web/app/themes/hybrid-demo/resources/views/components/alert.blade.php
+++ web/app/themes/hybrid-demo/resources/views/components/alert.blade.php
@@ -12,7 +12,15 @@
   };
 @endphp
 
-<div x-data x-init="console.log('Testing Alpine')"
-  {{ $attributes->merge(['class' => "px-sm py-xs rounded-sm {$class}"]) }}>
+<div x-data="{ open: true }" x-transition x-cloak x-show="open"
+  {{ $attributes->merge(['class' => "px-sm py-xs rounded-sm flex items-center justify-between gap-sm {$class}"]) }}>
   {!! $message ?? $slot !!}
+
+  <button class="cursor-pointer text-3xl" @click="open = false">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+      fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+      <path
+        d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+    </svg>
+  </button>
 </div>
